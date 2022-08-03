a5dda116eac5c9619c63c455027b4eb1bed1cb99
Matt Riley <mattrileyaus@gmail.com>
2022-08-03 20:28:56 +1000

    Unspecified changes

--- src/web-components.js
+++ src/web-components.js
@@ -1,14 +1,12 @@
 const compose = require('./compose');
 
-const composition = compose();
+const compositionName = 'Matt Riley web components';
+const composition = compose({ compositionName });
 const { webComponents } = composition.modules;
-if (!window.apps) window.apps = {};
-window.apps.mattrileyWebcomponents = composition;
+customElements.define('dev-bar', webComponents.DevBar);
+customElements.define('wip-bar', webComponents.WipBar);
 
 const fonts = document.createElement('link');
 fonts.href = 'https://fonts.googleapis.com/css?family=Roboto+Condensed|Shadows+Into+Light';
 fonts.rel = 'stylesheet';
 document.head.appendChild(fonts);
-
-customElements.define('dev-bar', webComponents.DevBar);
-customElements.define('wip-bar', webComponents.WipBar);
